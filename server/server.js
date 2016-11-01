00c9e4e0abbd8386e4ff54bc484958f743f32b90
HansCM <HansCMaschke@gmail.com>
2016-10-31 22:02:38 -0700

    Add post method

--- server/server.js
+++ server/server.js
@@ -11,6 +11,18 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.post('/todos',(req, res) => {
+	var todo = new Todo({
+		text: req.body.text
+	});
+	
+	todo.save().then((doc) => {
+		res.send(doc);
+	}, (e) => {
+		res.status(400).send(e);
+	});	
+});
+
 app.get("/todos/:id", (req,res) => {
 	var id = req.params.id;
 	if(!ObjectID.isValid(id)){
